edbedba3ba573829d7ec04af45af2a44df9aafa4
Jeremy Wilken <gnomation@gnomeontherun.com>
2014-12-09 14:35:03 -0600

    add view method to controller

--- js/editor.ctrl.js
+++ js/editor.ctrl.js
@@ -2,6 +2,11 @@ angular.module('App')
 .controller('EditorCtrl', function ($scope, $http) {
   $scope.editing = true;
 
+  $scope.view = function (index) {
+    $scope.editing = false;
+    $scope.content = $scope.notes[index];
+  };
+
   $http.get('/notes').success(function (data) {
     $scope.notes = data;
   }).error(function (err) {
