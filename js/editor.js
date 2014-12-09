5d56061bd99001bbca3d6a5b6d0063e28caf4401
Jeremy Wilken <gnomation@gnomeontherun.com>
2014-12-09 15:22:30 -0600

    setup editor for creating and editing notes

--- js/editor.js
+++ js/editor.js
@@ -7,6 +7,14 @@ angular.module('App')
     $scope.content = $scope.notes[index];
   };
 
+  $scope.create = function () {
+    $scope.editing = true;
+    $scope.content = {
+      title: '',
+      content: ''
+    };
+  };
+
   $http.get('/notes').success(function (data) {
     $scope.notes = data;
   }).error(function (err) {
