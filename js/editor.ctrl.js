49387927d2016102637ce5697c3021640d26a4de
Jeremy Wilken <gnomation@gnomeontherun.com>
2014-12-09 14:28:32 -0600

    load and display list of notes from service

--- js/editor.ctrl.js
+++ js/editor.ctrl.js
@@ -1,4 +1,11 @@
 angular.module('App')
-.controller('EditorCtrl', function ($scope) {
+.controller('EditorCtrl', function ($scope, $http) {
   $scope.editing = true;
+
+  $http.get('/notes').success(function (data) {
+    $scope.notes = data;
+  }).error(function (err) {
+    $scope.error = 'Could not load notes';
+  });
+
 });
